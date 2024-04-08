e95e35891110c209e131d8bd17d3a51858e93669
Kévin Casagrande <github.kcasagrande@xoxy.net>
2024-04-08 19:43:54 +0200

    List accounts

--- src/main/scala/net/mindbuilt/finances/api/v1/AccountController.scala
+++ src/main/scala/net/mindbuilt/finances/api/v1/AccountController.scala
@@ -6,7 +6,7 @@ import io.circe.literal._
 import io.circe.syntax._
 import net.mindbuilt.finances.api.v1.AccountController._
 import net.mindbuilt.finances.application.AccountService
-import net.mindbuilt.finances.business.{Holder, Iban}
+import net.mindbuilt.finances.business.{Account, Holder, Iban}
 import org.http4s.HttpRoutes
 import org.http4s.circe.CirceEntityEncoder
 import org.http4s.dsl.io._
@@ -16,6 +16,14 @@ class AccountController(
 ) extends CirceEntityEncoder
 {
   def apply(): HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case GET -> Root =>
+      accountService.getAllAccounts
+        .value
+        .flatMap {
+          case Left(throwable) => InternalServerError(throwable.getMessage)
+          case Right(accounts) => Ok(accounts.map(_.asJson))
+        }
+        
     case GET -> Root / IbanVar(account) / "holders" =>
       accountService.getHoldersByAccount(account)
         .value
@@ -33,6 +41,17 @@ object AccountController {
       Some(string)
         .filter(_.nonEmpty)
         .flatMap(Iban.fromString(_).toOption)
+  }
+  
+  implicit val accountEncoder: Encoder[Account] = Encoder.instance {
+    (account: Account) =>
+      json"""{
+                "iban": ${account.iban.toString},
+                "bic": ${account.bank.toString},
+                "domiciliation": ${account.domiciliation},
+                "holder": ${account.holder.name}
+                
+          }"""
   }
   
   implicit val holderEncoder: Encoder[Holder.Single] = Encoder.instance {
--- src/main/scala/net/mindbuilt/finances/application/AccountService.scala
+++ src/main/scala/net/mindbuilt/finances/application/AccountService.scala
@@ -2,11 +2,14 @@ package net.mindbuilt.finances.application
 
 import cats.data.EitherT
 import cats.effect.IO
-import net.mindbuilt.finances.business.{AccountRepository, Holder, Iban}
+import net.mindbuilt.finances.business.{Account, AccountRepository, Holder, Iban}
 
 class AccountService(
   accountRepository: AccountRepository
 ) {
+  def getAllAccounts: EitherT[IO, Throwable, Set[Account]] =
+    accountRepository.getAll
+    
   def getHoldersByAccount(account: Iban): EitherT[IO, Throwable, Set[Holder.Single]] =
     accountRepository.getByIban(account).map(_.map(_.individualHolders).getOrElse(Set.empty[Holder.Single]))
 }
