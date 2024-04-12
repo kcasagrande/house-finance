b6858a411e3c4ea3e27ad78fc48c16b3a74a6bd7
Kévin Casagrande <github.kcasagrande@xoxy.net>
2024-04-12 23:22:25 +0200

    Move the account selection to a dedicated component

---
+++ src/main/front/src/component/AccountChooser.js
@@ -0,0 +1,45 @@
+import configuration from '../Configuration';
+import { useEffect, useState } from 'react';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+
+function fetchAccounts(callback) {
+  return fetch(configuration.api + "/accounts")
+    .then(response => {
+      if(response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Response status is ' + response.status);
+      }
+    })
+    .then(callback);
+}
+
+function AccountChooser({onChange}) {
+  const [ready, setReady] = useState(false);
+  const [availableAccounts, setAvailableAccounts] = useState([]);
+  
+  useEffect(() => {
+    if(!ready) {
+      fetchAccounts(setAvailableAccounts)
+        .then(() => setReady(true));
+    }
+  }, [ready]);
+  
+  return (
+    <FormControl sx={{ width: 325 }}>
+      <InputLabel id="account-iban-select-label">Account</InputLabel>
+      <Select
+        id="account-iban-select"
+        labelId="account-iban-select-label"
+        label="Account"
+        onChange={event => onChange(event.target.value)}
+      >
+        {availableAccounts.map((account) =>
+          <MenuItem value={account}>{account.iban} - {account.holder}</MenuItem>
+        )}
+      </Select>
+    </FormControl>
+  );
+}
+
+export default AccountChooser;
--- src/main/front/src/tab/Import.js
+++ src/main/front/src/tab/Import.js
@@ -2,47 +2,29 @@ import configuration from '../Configuration';
 import { useEffect, useState, useRef } from 'react';
 import { Box, Button, CircularProgress, Container, FormControl, Input, InputLabel, MenuItem, Select, Stack, Step, StepButton, StepLabel, Stepper, TextField } from '@mui/material';
 import { FileUpload } from '@mui/icons-material';
+import AccountChooser from '../component/AccountChooser';
 import ImportReview from '../component/ImportReview';
 
 function Import() {
   const [status, setStatus] = useState('initializing');
-  const [availableAccounts, setAvailableAccounts] = useState([]);
-  const [account, setAccount] = useState('');
+  const [selectedAccount, setSelectedAccount] = useState(null);
   const [selectedFile, setSelectedFile] = useState('');
   const [parsedRows, setParsedRows] = useState([]);
   const fileInput = useRef();
 
-  useEffect(() => {
-    if(status === 'initializing') {
-      fetchAccounts()
-        .finally(() => setStatus('pending'));
-    }
-  }, []);
-
-  function fetchAccounts() {
-    return fetch(configuration.api + "/accounts")
-      .then(response => {
-        if(response.ok) {
-          return response.json()
-        } else {
-          throw new Error('Response status is ' + response.status);
-        }
-      })
-      .then(setAvailableAccounts);
-  }
-
   function changeSelectedFile(file) {
     setSelectedFile(file);
-    process(account, file);
+    process(selectedAccount, file);
   }
   
-  function changeAccount(iban) {
-    setAccount(iban);
-    process(iban, selectedFile);
+  function changeSelectedAccount(account) {
+    setSelectedAccount(account);
+    process(account, selectedFile);
+  }
   }
 
-  function process(iban, file) {
-    if(!!iban && !!file) {
+  function process(account, file) {
+    if(!!account && !!file) {
       setStatus('parsing');
       const formData = new FormData();
       formData.append('statement', file);
@@ -63,14 +45,7 @@ function Import() {
     <Container fixed>
       <Stack direction="column" spacing={2} useFlexGap={true}>
         <Stack direction="row" alignItems="flex-end" justifyContent="center" spacing={2} useFlexGap={true}>
-          <FormControl sx={{ width: 325 }}>
-            <InputLabel id="account-iban-select-label">Account IBAN</InputLabel>
-            <Select id="account-iban-select" labelId="account-iban-select-label" label="Account IBAN" onChange={event => changeAccount(event.target.value.iban)}>
-              {availableAccounts.map((account) =>
-                <MenuItem value={account}>{account.iban} - {account.holder}</MenuItem>
-              )}
-            </Select>
-          </FormControl>
+          <AccountChooser onChange={changeSelectedAccount} />
           <input ref={fileInput} type="file" accept="text/csv,.csv" onChange={event => changeSelectedFile(event.target.files[0])} style={{display: 'none'}} />
           <Button variant="outlined" onClick={() => fileInput.current.click()}>Select file</Button>
           <TextField variant="standard" InputProps={{readOnly: true}} value={selectedFile.name}></TextField>
