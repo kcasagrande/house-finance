c0bf64c8289778decc54854440a05f0aa53ca715
Kévin Casagrande <github.kcasagrande@xoxy.net>
2024-05-09 14:30:43 +0200

    Add a table to view the details of the repartition

---
+++ src/main/front/src/component/report/CategorySupplierTable.js
@@ -0,0 +1,72 @@
+import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { amount } from '../../format';
+import './CategorySupplierTable.css';
+
+function AmountCell({value, total, ...props}) {
+  return (
+    <TableCell {...props} align="center">{amount(value)}</TableCell>
+  );
+}
+
+function sum(total, operand) {
+  return total + operand;
+}
+
+function CategorySupplierTable({operations, holders}) {
+  const data = operations
+    .flatMap((operation) => operation.breakdown)
+    .reduce((_data, breakdown) => {
+      return {
+        ..._data,
+        [breakdown.category || '']: {
+          ...(_data?.[breakdown.category || ''] || {}),
+          [breakdown.supplier || '']: (_data?.[breakdown.category || '']?.[breakdown.supplier || ''] || 0) + breakdown.credit
+        }
+      };
+    }, {});
+    
+  return (
+    <Table size="small">
+      <TableHead>
+        <TableRow>
+          <TableCell key=""></TableCell>
+          <TableCell className="total" key="TOTAL" align="center">TOTAL</TableCell>
+          {holders.map((holder) =>
+              <TableCell key={holder.id} align="center">{holder.name}</TableCell>
+          )}
+          <TableCell key="-" align="center">Non assigné</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow hover>
+          <TableCell className="total" key={'TOTAL'} align="right" variant="head">TOTAL</TableCell>
+          <AmountCell className="total" key={'TOTAL-TOTAL'} value={Object.keys(data).map((category) => Object.keys(data[category]).map((holder) => data[category]?.[holder] || 0).reduce(sum, 0)).reduce(sum, 0)} />
+          {holders.map((holder) =>
+            <AmountCell className="total" key={'TOTAL-' + holder.id} value={Object.keys(data).map((category) => data?.[category]?.[holder.id] || 0).reduce(sum, 0)} />
+          )}
+          <AmountCell className="total" key={'TOTAL-'} value={Object.keys(data).map((category) => data?.[category]?.[''] || 0).reduce(sum, 0)} />
+        </TableRow>
+        {Object.keys(data).toSorted().filter(category => !!category).map((category) =>
+          <TableRow hover>
+            <TableCell key={category} align="right" variant="head">{category || 'Non catégorisé'}</TableCell>
+            <AmountCell className="total" key={category + '-TOTAL'} value={Object.keys(data[category]).map((holder) => data[category]?.[holder] || 0).reduce(sum, 0)} />
+            {holders.map((holder) =>
+              <AmountCell key={category + '-' + holder.id} value={data?.[category]?.[holder.id] || 0} />
+            )}
+            <AmountCell key={category + '-'} value={data?.[category]?.[''] || 0} />
+          </TableRow>
+        )}
+        <TableRow hover>
+          <TableCell key={''} align="right" variant="head">Non catégorisé</TableCell>
+          <AmountCell className="total" key={'-TOTAL'} value={Object.keys(data[''] || {}).map((holder) => data['']?.[holder] || 0).reduce(sum, 0)} />
+          {holders.map((holder) =>
+            <AmountCell key={'-' + holder.id} value={data?.['']?.[holder.id] || 0} />
+          )}
+          <AmountCell key={'-'} value={data?.['']?.[''] || 0} />
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+}
+
+export default CategorySupplierTable;
--- src/main/front/src/tab/Reports.js
+++ src/main/front/src/tab/Reports.js
@@ -1,4 +1,6 @@
+import Grid from '@mui/material/Unstable_Grid2';
 import CategoryExpenses from '../component/report/CategoryExpenses';
+import CategorySupplierTable from '../component/report/CategorySupplierTable';
 import SuppliersRepartition from '../component/report/SuppliersRepartition';
 
 function Reports({operations, holders}) {
@@ -6,10 +8,17 @@ function Reports({operations, holders}) {
   const creditOperations = operations.filter((operation) => operation.credit > 0);
   
   return (
-    <>
-      <CategoryExpenses operations={debitOperations} />
-      <SuppliersRepartition operations={debitOperations} categories={['Alimentaire', 'Essence']} holders={holders} />
-    </>
+    <Grid container spacing={2} columns={3}>
+      <Grid item xs>
+        <CategorySupplierTable operations={debitOperations} holders={holders} />
+      </Grid>
+      <Grid item xs>
+        <CategoryExpenses operations={debitOperations} width={500} />
+      </Grid>
+      <Grid item xs>
+        <SuppliersRepartition operations={debitOperations} categories={['Alimentaire', 'Essence']} holders={holders} width={500} />
+      </Grid>
+    </Grid>
   );
 }
 
